ab5d962390ff379ab2df2966413bba82f9acdfa1
krknw <ki12248@auca.kg>
2026-09-22 10:43:17 +0600

    add swagger implementation

--- BankAccSim/Program.cs
+++ BankAccSim/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
+
+app.MapControllers();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
